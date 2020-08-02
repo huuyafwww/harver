1ca6d4ff2abdf865f7e9ccf1be3247db2d273cf7
huuyafwww <huuya1234fwww@gmail.com>
2020-08-02 14:50:38 +0900

    Add Variable Component Toggle of Dynamic Component

---
+++ src/components/main/home/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import styled from 'styled-components';
+
+const HomeWrapper = styled.div``;
+
+@inject('store')
+@observer
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <HomeWrapper>
+                <h1>Homeページ</h1>
+            </HomeWrapper>
+        );
+    }
+}
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import styled from 'styled-components';
+import Home from '@components/main/home';
 
 const MainWrapper = styled.div``;
+
+const Components = [Home];
 
 @inject('store')
 @observer
@@ -10,18 +13,40 @@ export default class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {};
-        this.store = this.props.store;
+        this.nowPageLabel = this.props.store.nowPageLabel;
+        this.getNowPageComponent(this.nowPageLabel);
+    }
+
+    getNowPageComponent(nowPageLabel) {
+        for (const Component of Components) {
+            const ComponentName = this.getComponentName(Component);
+            if (nowPageLabel === ComponentName) {
+                this.setNowPageComponent(Component);
+                break;
+            }
+        }
+    }
+
+    getComponentName(Component) {
+        return Component.wrappedComponent.name;
+    }
+
+    setNowPageComponent(Component) {
+        this.NowPageComponent = Component;
     }
 
     render() {
-        const { nowPageLabel } = this.store;
+        const { nowPageLabel } = this;
+        const { NowPageComponent } = this;
         return (
             <MainWrapper className="main-content">
                 <section className="section">
                     <div className="section-header">
                         <h1>{nowPageLabel}</h1>
                     </div>
-                    <div className="section-body"></div>
+                    <div className="section-body">
+                        <NowPageComponent />
+                    </div>
                 </section>
             </MainWrapper>
         );
